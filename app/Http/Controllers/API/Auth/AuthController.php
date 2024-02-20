5b1822e1e493b5c3a31a356f0adfdbc528c746cd
Mochammad Ilham Adrian <muhamadilhamadrian0@gmail.com>
2024-02-20 12:43:03 +0700

    initial push

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $token = $request->user()->createToken('NamaToken')->plainTextToken;
+            return response()->json(['token' => $token]);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Jika registrasi berhasil, kembalikan token API untuk pengguna yang terdaftar
+        $token = $user->createToken('NamaToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+
+}
